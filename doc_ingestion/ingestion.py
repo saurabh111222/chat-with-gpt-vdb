c99d6c9351cb41653e0543b310ad64b827f3a6e2
saurabh111222 <sk3144414@gmail.com>
2023-09-04 23:00:58 +0530

    updated ingestion

--- doc_ingestion/ingestion.py
+++ doc_ingestion/ingestion.py
@@ -1,6 +1,5 @@
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
 
 from langchain.document_loaders import PyPDFLoader
@@ -10,58 +9,9 @@ from langchain.text_splitter import (
     TextSplitter,
 )
 from langchain.embeddings.openai import OpenAIEmbeddings
-# from utils.vdb_connections import get_client, get_schema
+from utils.vdb_connections import get_client, get_schema
 from langchain.vectorstores import Weaviate
 import weaviate
-
-
-def get_client():
-    client = weaviate.Client(url=os.environ.get("WEAVIATE_URL"), 
-                         auth_client_secret=weaviate.AuthApiKey(os.environ.get("WEAVIATE_API_KEY")), 
-                         additional_headers = {
-                                "X-OpenAI-Api-Key": f"{os.environ.get('OPENAI_API_KEY')}"  # Replace with your inference API key
-                    })
-    return client
-
-def get_schema(class_name:str):
-    if class_name == 'Current_affairs':
-        class_obj = {
-            "class": f"{class_name}",
-            "description": "An Author class to store the author information",
-            "properties": [
-                {
-                    "name": "content",
-                    "dataType": ["text"],
-                    "description": "The actual content", 
-                },
-                {
-                    "name": "metadata",
-                    "dataType": ["text"],
-                    "description": "metadata about the content", 
-                }
-            ],
-            "vectorizer": "text2vec-openai"
-        }
-    elif class_name == 'Bill_passed':
-         class_obj = {
-            "class": f"{class_name}",
-            "description": "An Author class to store the author information",
-            "properties": [
-                {
-                    "name": "content",
-                    "dataType": ["text"],
-                    "description": "The actual content", 
-                },
-                {
-                    "name": "metadata",
-                    "dataType": ["text"],
-                    "description": "metadata about the content", 
-                }
-            ],
-            "vectorizer": "text2vec-openai"
-        }
-    return class_obj
-
 
 
 # weaviate client
@@ -87,11 +37,10 @@ def ingest_doc(QUERY:str, SUBJECT: str):
     class_obj = get_schema(class_name=SUBJECT)
     print(class_obj)
     embedding = OpenAIEmbeddings()
-    # client.schema.delete_all()
 
     if client.schema.exists(SUBJECT):
         print(f"Data for class {SUBJECT} already exists")
-        vectorstore = Weaviate(client, f"{SUBJECT}", "content", attributes=["source"], embedding=embedding)
+        # vectorstore = Weaviate(client, f"{SUBJECT}", "content", attributes=["source"], embedding=embedding)
 
     else:
         print(f"Class object {SUBJECT} does not exist.\n Creating {SUBJECT}...")
@@ -107,10 +56,10 @@ def ingest_doc(QUERY:str, SUBJECT: str):
         vectorstore.add_texts(texts, meta)
         print(f"Inserted {len(texts)} documents into db.")
     
-    doc = vectorstore.similarity_search_with_score(query=QUERY, by_text=False)
-    similarity_score = doc[0][1]
-    print(f"Splitted into {len(texts)} chunks.")
-    return similarity_score
+    # doc = vectorstore.similarity_search_with_score(query=QUERY, by_text=False)
+    # similarity_score = doc[0][1]
+    # print(f"Splitted into {len(texts)} chunks.")
+    # return similarity_score
 
         
 
--- main.py
+++ main.py
@@ -2,20 +2,23 @@ from dotenv import load_dotenv
 import streamlit as st
 from streamlit_chat import message
 from llm_models.core import run_llm
-import uuid
-
-
+from utils.vdb_connections import generate_unique_uuid
 load_dotenv()
+import time
+#Setting header
 st.header("ChatWithGPT")
+
+#Taking subject
 option = st.selectbox(
     'Select Subject',
     ('Current affairs', 'Bill passed'))
 
-def generate_unique_uuid():
-    return str(uuid.uuid4())
-
+#Taking prompt query
+# with st.form(key='my_form'):
+# 	prompt = st.text_input("Prompt", placeholder="Enter your query here...",  key='widget')
+# 	submit_button = st.form_submit_button(label='Submit')
+     
 prompt = st.text_input("Prompt", placeholder="Enter your query here...",  key='widget')
-
 
 
 if "user_prompt_history" not in st.session_state:
@@ -28,22 +31,23 @@ if "chat_history" not in st.session_state:
 
 if prompt:
     with st.spinner('Processing query...'):
+        time.sleep(30)
+        
         SUBJECT = option.replace(' ', '_')
         print(f'Select option: {option} and subject: {SUBJECT}')
         generated_response, similarity_score = run_llm(QUERY=prompt, SUBJECT=SUBJECT, CHAT_HISTORY=st.session_state['chat_history'])
-        # print(generated_response)
-        formated_response = F"{generated_response['answer']} \n\n Similarity score: {similarity_score}"
-
-        # print(st.session_state)
-        # print(st.session_state['chat_history'])
+        if "don't know" not in generated_response['answer']:
+           formated_response = F"{generated_response['answer']} \n\n Similarity score: {similarity_score}"
+        else:
+            formated_response=generated_response['answer']
         st.session_state["chat_answers_history"].append(formated_response)
         st.session_state["user_prompt_history"].append(prompt)
         st.session_state["chat_history"].append((prompt, generated_response["answer"]))
 
 if st.session_state["chat_answers_history"]:
-    for generated_response, user_query in zip(
+    for gen_response, user_query in zip(
         st.session_state["chat_answers_history"], 
         st.session_state["user_prompt_history"]):
         # message(user_query, is_user=True)
-        message(generated_response, key=f'{generate_unique_uuid()}')
+        message(gen_response, key=f'{generate_unique_uuid()}')
 
